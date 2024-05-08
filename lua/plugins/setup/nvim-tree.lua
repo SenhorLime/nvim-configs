04a8332e00fdd1e60dfe1ac95c03d608e0987abb
João Vitor Lima <108885897+SenhorLime@users.noreply.github.com>
2024-05-08 18:01:15 -0300

    Configurando o NvimTree

--- lua/plugins/setup/nvim-tree.lua
+++ lua/plugins/setup/nvim-tree.lua
@@ -1,1 +1,31 @@
-require('nvim-tree').setup()
+require('nvim-tree').setup({
+  renderer = {
+    full_name = true,
+    group_empty = true,
+    root_folder_label = false,
+    highlight_opened_files = 'all',
+    indent_markers = {
+      enable = true,
+    },
+    icons = {
+      glyphs = {
+        git = {
+          unstaged = ' ',
+          staged = ' ',
+          unmerged = ' ',
+          renamed = ' ',
+          untracked = '󱨧 ',
+          deleted = ' ',
+          ignored = '◌',
+        },
+      },
+    },
+  },
+  filters = {
+    custom = {
+      '^.git$',
+      '.vscode/',
+      '.idea/',
+    },
+  }
+})
